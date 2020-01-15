105ae9bbd9c1e9f924de2d1002e31e998bfe808b
givebest <givenlovs@msn.com>
2020-01-15 14:55:28 +0800

    +init

---
+++ README.md
@@ -0,0 +1,85 @@
+# GB-Pagination
+----
+
+## 简介
+
+Node.js 服务端渲染（SSR）分页。
+
+## 使用
+
+### 普通分页
+
+```js
+// 引入
+const gbPagination = require('gb-pagination.js')
+
+// 配置
+gbPagination.init({
+    total: 101, // 总记录数
+    curPage: 1,  // 当前页
+    size: 5  // 每页显示数量
+})
+
+// 分页渲染
+console.log(gbPagination.build())
+```
+
+### Express.js + EJS 模板 中使用
+
+#### JS
+
+```js
+var express = require('express');
+var router = express.Router();
+
+const gbPagination = require('pagination.js')
+
+
+router.get('/:p', function(req, res, next) {
+
+  const curPage = req.params.p || 1
+
+  gbPagination.init({
+    total: 101,
+    curPage: 1,
+    size: 5,
+  })
+
+  res.render('index', { title: 'Express',  pagination: gbPagination.build()});
+});
+```
+
+#### EJS 
+
+```html
+<link rel='stylesheet' href='gb-pagination.css' />
+<div class="gb-pagination">
+	<%- pagination %>
+</div>
+```
+
+## 选项
+
+- `total`: 总记录数      
+- `curPage`: 当前页码
+- `size`:  每页记录数 || 10
+- `prevText`:  上一页文案 || '上一页'，值为 false 不显示
+- `nextText`: '下一页文案 || '下一页' 值为 false 不显示
+- `pageInfo`: 分页信息，{{currentPage}}当前页码，{{totalPages}}总页数 || 当前第<b>{{currentPage}}</b>页/共<b>{{totalPages}}</b>页，值为 false 不显示
+
+
+
+## 感谢
+
+> 本项目代码由 `GB-paging` 改进而来。
+
+### GB-paging
+> 兼容IE6+及现代浏览器的简单分页，支持同一页面多个分页。 
+
+[https://github.com/givebest/GB-paging](https://github.com/givebest/GB-paging)    
+
+
+
+## License
+
+[MIT](./LICENSE) © 2020 [givebest](https://github.com/givebest)
---
+++ gb-pagination.js
@@ -0,0 +1,113 @@
+let defaultConfig,
+    args;
+
+/**
+ * 默认配置
+ */
+defaultConfig = {
+    curPage: 1, //当前页           
+    size: 10, //每页n条
+    prevText: '上一页',
+    nextText: '下一页',
+    pageInfo: '当前第<b>{{currentPage}}</b>页/共<b>{{totalPages}}</b>页',
+    goUrl: '/{{n}}'
+}
+
+function GbPagination() {
+    if (!(this instanceof GbPagination)) return new GbPagination();
+}
+
+/**
+ * 初始化
+ */
+GbPagination.prototype.init = function(opts) {
+    this.getParams(opts);
+}
+
+/**
+ * 获取分页参数
+ */
+GbPagination.prototype.getParams = function(opts) {
+    args = this.opts = opts || {};
+
+    args.total = args.total || 0;
+    args.curPage = args.curPage || defaultConfig.curPage;
+    args.size = args.size || defaultConfig.size;
+    args.goUrl = args.goUrl || defaultConfig.goUrl;
+    args.pages = Math.ceil(args.total / args.size || 0);
+    args.isPrev = (args.prevText !== false) ? true : false;
+    args.prevText = args.prevText || defaultConfig.prevText;
+    args.isNext = (args.nextText !== false) ? true : false;
+    args.nextText = args.nextText || defaultConfig.nextText;
+    args.isPageInfo = (args.pageInfo !== false) ? true : false;
+    args.pageInfo = args.pageInfo || defaultConfig.pageInfo;
+}
+
+/**
+ * 分页html渲染
+ */
+GbPagination.prototype.build = function() {
+    var html = [];
+    // console.log('build', args)
+
+    // 没有数据
+    if (args.pages <= 1) {
+        addClass(args.container, 'gb-hide');
+        return;
+    }
+
+    if (args.curPage >>> 0 <= 0) return;
+    args.curPage = args.curPage >>> 0
+
+    var ii = (args.curPage == 1 || args.curPage == args.pages) ? 3 : 2;
+    var urlParam = parseUrl(args.goUrl);
+
+    html.push('<a href="' + urlParam + args.curPage + '" class="gb-pagination-current">' + args.curPage + '</a>');
+
+    for (var i = 1; i < ii; i++) {
+        if (args.curPage - i > 1) {
+            html.unshift('<a href="' + urlParam + (args.curPage - i) + '">' + (args.curPage - i) + '</a>');
+        }
+
+        if (args.curPage + i < args.pages) {
+            html.push('<a href="' + urlParam + (args.curPage + i) + '">' + (args.curPage + i) + '</a>');
+        }
+    }
+
+    if (args.curPage - 2 > 1) {
+        html.unshift('<span class="gb-pagination-ellipsis">...</span>');
+    }
+
+    if (args.curPage > 1) {
+        html.unshift('<a href="' + urlParam + '1">1</a>');
+        args.isPrev && html.unshift('<a href="' + urlParam + (args.curPage - 1) + '">' + args.prevText + '</a>');
+    } else {
+        args.isPrev && html.unshift('<a class="disabled">' + args.prevText + '</a>');
+    }
+
+    if (args.curPage + 2 < args.pages) {
+        html.push('<span class="gb-pagination-ellipsis">...</span>');
+    }
+
+    if (args.curPage < args.pages) {
+        html.push('<a href="' + urlParam + args.pages + '">' + args.pages + '</a>');
+        args.isNext && html.push('<a href="' + urlParam + (args.curPage + 1) + '">' + args.nextText + '</a>');
+    } else {
+        args.isNext && html.push('<a class="disabled">' + args.nextText + '</a>');
+    }
+
+    // 是否显示右侧分页信息
+    if (args.isPageInfo) html.push('<div class="gb-pagination-info">' + args.pageInfo.replace('{{currentPage}}', args.curPage).replace('{{totalPages}}', args.pages) + '</div>');
+
+    // args.container.innerHTML = html.join('');
+    return html.join('')
+}
+
+/**
+ * 解析URL
+ */
+function parseUrl(url) {
+    return url ? url.replace('{{n}}', '') : param
+}
+
+module.exports = GbPagination()
